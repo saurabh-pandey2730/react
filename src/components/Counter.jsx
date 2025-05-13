b3bbb22b2e97fe1304ea6d37e975c5faf983e0a3
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2025-05-13 18:48:11 +0530

    navbar css changed (tailwind) & icons

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-
+import { FaPlusCircle } from "react-icons/fa"; 
+import { FaCircleMinus } from "react-icons/fa6";
 const Counter = () => {
 
         const [number,setNumber]  = useState(0)
@@ -10,8 +11,8 @@ const Counter = () => {
           // setNumber(number+1)
           // setNumber(number+1)
            setNumber((prev)=>prev+1)
-           setNumber((prev)=>prev+1)
-           setNumber((prev)=>prev+1)
+          //  setNumber((prev)=>prev+1)
+          //  setNumber((prev)=>prev+1)
           // console.log('after setFun',number)
         }
       useEffect(()=>{
@@ -24,8 +25,8 @@ const Counter = () => {
         <h1>Counter APP</h1>
          <h1 className="text-3xl ml-24 mb-5">{number}</h1>
 
-        <button className="btn-data" onClick={IncreaseNumber}>Increase+</button>
-        <button className="btn-data" onClick={()=>setNumber(number-1)}>Decrease-</button>
+        <button className="btn-data" onClick={IncreaseNumber}><FaPlusCircle/></button>
+        <button className="btn-data" onClick={()=>setNumber(number-1)}> <FaCircleMinus/>   </button>
 
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import  './Navbar.css'
 import MyContext from './context/UseContext'
-
+import { FaRegUserCircle } from "react-icons/fa";
 
 
 const Navbar = () => {
@@ -9,18 +9,18 @@ const Navbar = () => {
     let {imgUrl, itemsData}=useContext(MyContext)
 
   return (
-    <div className="navbar">
-       <img src={imgUrl} alt="" />
-          <div className='menu-items'>
+    <div className=" h-[66px] bg-[#734eed] flex justify-around items-center">
+       <img src={imgUrl} alt=""  className='w-33' />
+          <div className='w-[500px] flex justify-around items-center'>
             {
               itemsData.map((data, key)=>{
-                return <a key={key} href="">{data}</a>
+                return <a key={key} href="" className='text-xl font-extrabold no-underline'>{data}</a>
               })
             }
           </div>
-          <div className="btn">
-            <button className=''>Login</button>
-            <button>Register</button>
+          <div >
+            <button className='ml-5 bg-[#1bdf18] rounded p-2 w-20 '>Login</button>
+            <button className='ml-5 bg-[#1bdf18] rounded p-2 w-20'>Register</button>
           </div>
     </div>
   )
